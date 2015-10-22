75cb4732735fb2c36ce1cbf50bf16af57b1ab7d6
Lauryn <lryndavis@gmail.com>
2015-10-22 14:00:30 -0700

    passes 2nd spec: ensures proper return for 1

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,16 @@
 var bottlesOfBeer = function(number) {
   if (number === 0) { 
     return 'No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.'; 
-  }
+  } else if (number === 1) { 
+    return 'One bottle of beer on the wall, one bottle of beer. Take one down and pass it around, one bottle of beer on the wall.';
+  } 
 }
+
+
+
+
+
+
+
+
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('bottlesOfBeer', function() {
   it("will show that there are no more bottles of beer at 0", function() {
-    expect(bottlesOfBeer(0)).to.equal('No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.');
+    expect(bottlesOfBeer(0)).to.equal('No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.')
+  });
+
+  it("will show the proper output for a single bottle of beer", function() {
+    expect(bottlesOfBeer(1)).to.equal('One bottle of beer on the wall, one bottle of beer. Take one down and pass it around, one bottle of beer on the wall.')
   });
 });
